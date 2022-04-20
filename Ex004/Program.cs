36720e5b2f690d8c1c2e5eac2dea98f45cb9f29e
Nadezhda Cheremnyh <chni2707@mail.ru>
2022-04-21 01:07:08 +0500

    Задача 3

---
+++ Ex004/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 3 Напишите программу, которая принимает на вход число и выдает, является ли число четным 
+// (делится ли оно на два без остатка)
+int count = 1;
+int n = 4;
+while (count < n)
+{
+    Console.Write("Введите число: ");
+    int A = int.Parse(Console.ReadLine());
+    int Ostatok = (A % 2);
+
+    if (Ostatok == 0) Console.WriteLine("да");
+    else Console.WriteLine("нет");
+
+    count = count + 1;
+}
