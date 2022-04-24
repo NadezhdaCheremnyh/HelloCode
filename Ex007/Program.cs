5264a1ef7d46638244dd96da3515414ee5df7d35
Nadezhda Cheremnyh <chni2707@mail.ru>
2022-04-24 22:30:31 +0500

    Lesson 2 Task 2

---
+++ Ex007/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 6 Напишите программу, которая на вход принимает трехзначное число, 
+// а на выходе показывает последнюю цифру этого числа 
+
+// int count = 1;
+// int n = 4;
+// while (count < n)
+// {
+Console.WriteLine("Введите трехзначное число: ");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLite(N);
+// string znach = stroka.Substr(3, 1);
+// Console.WriteLite(znach);
+// count = count + 1;
+//}
---
+++ Ex008/Program.cs
@@ -0,0 +1,21 @@
+﻿// Lesson 2 Task 2 Напишите программу, которая принимает на вход трехзначное число 
+// и на выходе показывает вторую цифру этого числа
+int count = 1;
+int n = 4;
+string text = "result:  ";
+while (count < n)
+{
+    Console.Write("Введите трехзначное число: ");
+    int N = int.Parse(Console.ReadLine());
+    // Console.WriteLine(N);
+    if (N > 99 && N < 1000)
+    {
+        string stroka = Convert.ToString(N);
+        // Console.WriteLine(stroka);
+        string result = stroka.Substring(1, 1);
+        Console.WriteLine(text + result);
+        count++;
+    }
+    else Console.WriteLine("Введенное число не трехзначное");
+
+}
