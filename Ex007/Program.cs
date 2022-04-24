21f3bd9d72c691411cec337c47d56c01cbc3a405
NadezhdaCheremnyh <103581502+NadezhdaCheremnyh@users.noreply.github.com>
2022-04-24 22:44:19 +0500

    Update Задача 6 Урок 1

--- Ex007/Program.cs
+++ Ex007/Program.cs
@@ -1,14 +1,22 @@
 ﻿// Задача 6 Напишите программу, которая на вход принимает трехзначное число, 
 // а на выходе показывает последнюю цифру этого числа 
 
-// int count = 1;
-// int n = 4;
-// while (count < n)
-// {
-Console.WriteLine("Введите трехзначное число: ");
-int N = int.Parse(Console.ReadLine());
-Console.WriteLite(N);
-// string znach = stroka.Substr(3, 1);
-// Console.WriteLite(znach);
-// count = count + 1;
-//}
+int count = 1;
+int n = 4;
+string text = "result:  ";
+while (count < n)
+{
+    Console.Write("Введите трехзначное число: ");
+    int N = int.Parse(Console.ReadLine());
+    // Console.WriteLine(N);
+    if (N > 99 && N < 1000)
+    {
+        string stroka = Convert.ToString(N);
+        // Console.WriteLine(stroka);
+        string result = stroka.Substring(2, 1);
+        Console.WriteLine(text + result);
+        count++;
+    }
+    else Console.WriteLine("Введенное число не трехзначное");
+
+}
