9bf5d19a9c3b347501b1b0139f87d958bbde6303
Nadezhda Cheremnyh <chni2707@mail.ru>
2022-04-25 14:20:17 +0500

    Lesson 2 Task 4

---
+++ Ex010/Program.cs
@@ -0,0 +1,22 @@
+﻿// Lesson 2 Task 4 Напишите программу, которая принимает на вход число 
+// и проверяет, кратно ли оно одновременно 7 и 23
+
+string GetText(int x)
+{
+    int A = 7;
+    int B = 23;
+    string sl = "";
+    if ((x % A) == 0 && (x % B) == 0) sl = "да ";
+    else sl = "нет";
+    return sl;
+}
+int count = 1;
+int n = 4;
+while (count < n)
+{
+    Console.Write("Введите число: ");
+    int value = int.Parse(Console.ReadLine());
+    Console.WriteLine(GetText(value));
+
+    count++;
+}
