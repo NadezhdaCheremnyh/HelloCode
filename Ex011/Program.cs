9dc4021b8a329932407c685a4aea53d553b06792
Nadezhda Cheremnyh <chni2707@mail.ru>
2022-04-25 15:14:52 +0500

    Lesson 2 Task 8

---
+++ Ex011/Program.cs
@@ -0,0 +1,23 @@
+﻿// Lesson 2 Task 8 Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным
+
+string GetText(int x)
+{
+    string sl = "";
+    if ((x == 6) || (x == 7)) sl = "да ";
+    else sl = "нет";
+    return sl;
+}
+int count = 1;
+int n = 4;
+while (count < n)
+{
+    Console.Write("Введите цифру: ");
+    int value = int.Parse(Console.ReadLine());
+    if (value > 0 && value < 8)
+    {
+        Console.WriteLine(GetText(value));
+        count++;
+    }
+    else Console.WriteLine("Такого дня недели нет");
+}
