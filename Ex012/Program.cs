404e70cecf2f4160f36f2c2c7ab98435b72c7fa2
Nadezhda Cheremnyh <chni2707@mail.ru>
2022-04-27 22:20:46 +0500

    Lesson 3 Task 1

---
+++ Ex012/Program.cs
@@ -0,0 +1,33 @@
+﻿// Lesson 3 Task 1 Напишите программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом (перевертышем)
+
+string GetText(string s)
+{
+    string result = string.Empty;
+    int length = s.Length;
+    int index = 0;
+    int j = length;
+    for (int i = 0; (s[i] == s[j - 1] && (i < j - 1)); i++)
+    {
+        j = j - 1;
+        index = i;
+    }
+    if (index == (length / 2) - 1) result = "да ";
+    else result = "нет";
+    return result;
+}
+int count = 1;
+int n = 4;
+while (count < n)
+{
+    Console.Write("Введите пятизначное число: ");
+    int value = int.Parse(Console.ReadLine());
+    if (value > 10000 && value < 100000)
+    {
+        string stroka = Convert.ToString(value);
+        Console.Write(stroka + " -> ");
+        Console.WriteLine(GetText(stroka));
+        count++;
+    }
+    else Console.WriteLine("Введенное число не является пятизначным");
+}
