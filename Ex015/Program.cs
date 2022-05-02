5cf557510ef6d93815e389afc349e2210063e7b3
Nadezhda Cheremnyh <chni2707@mail.ru>
2022-05-02 13:05:53 +0500

    Lesson 4 Task 1

---
+++ Ex015/Program.cs
@@ -0,0 +1,28 @@
+﻿// Lesson 4 Task 1 Напишите программу, которая принимает на вход число A
+// и выдает сумму чисел от 1 до A
+
+int SummaNumbers(int x)
+{
+    int summa = 0;
+    for (int i = 1; i <= x; i++)
+    {
+        summa = summa + i;
+
+    }
+    return summa;
+}
+int count = 1;
+int n = 5;
+int value = 0;
+while (count < n)
+{
+    Console.Write("Введите целое число: ");
+    value = int.Parse(Console.ReadLine());
+    if (value > 0)
+    {
+        Console.Write("Сумма чисел от 1 до " + value + " => ");
+        Console.WriteLine(SummaNumbers(value));
+        count++;
+    }
+    else Console.WriteLine("Введенное значение не является целым");
+}
